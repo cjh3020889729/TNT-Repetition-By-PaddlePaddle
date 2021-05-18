719a2dd94effaf25aebb8062dac7dd2c868cbd6e
Jinghui Cai <55022974+cjh3020889729@users.noreply.github.com>
2021-05-19 01:10:18 +0800

    Update tnt_layers.py

--- models/tnt_layers.py
+++ models/tnt_layers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# @Author: 二月三
+# @Author: 喝鱼汤的鱼儿
 # @Date:   2021-05-18 10:11:30
-# @Last Modified by:   二月三
+# @Last Modified by:   喝鱼汤的鱼儿
 # @Last Modified time: 2021-05-18 22:24:34
 import paddle
 from paddle import nn
